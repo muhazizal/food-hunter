77b7a61f08f29fbd338187d15c78aeea3101f2d0
muhazizal <muhazizal.26@gmail.com>
2020-10-15 18:57:57 +0700

    update restaurant pages style responsive on tablet

--- src/scripts/views/components/restaurant/menu.js
+++ src/scripts/views/components/restaurant/menu.js
@@ -25,12 +25,12 @@ class Menu extends HTMLElement {
 				<div class="menu__inner">
 					<h2 class="menu__title">Our Menus</h2>
 					<div class="menu__foods">
-						<h3 class="food__title">Foods:</h3>
+						<h3 class="food__title">Foods</h3>
 						<ul class="food__list">
 						</ul>
 					</div>
 					<div class="menu__drinks">
-						<h3 class="drink__title">Drinks:</h3>
+						<h3 class="drink__title">Drinks</h3>
 						<ul class="drink__list">
 						</ul>
 					</div>
