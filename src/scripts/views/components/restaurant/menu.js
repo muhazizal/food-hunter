3aefe7f3782baf0f3be29a26e8ff48a0b813bda3
muhazizal <muhazizal.26@gmail.com>
2020-10-15 07:22:59 +0700

    update menu styles

--- src/scripts/views/components/restaurant/menu.js
+++ src/scripts/views/components/restaurant/menu.js
@@ -22,17 +22,19 @@ class Menu extends HTMLElement {
 
 		this.shadowDOM.innerHTML = `
 			<section class="menu">
-				<h2 class="menu__title">Our Menus</h2>
-        <div class="menu__foods">
-					<h3 class="food__title">Foods:</h3>
-					<ul class="food__list">
-					</ul>
-        </div>
-        <div class="menu__drinks">
-					<h3 class="drink__title">Drinks:</h3>
-					<ul class="drink__list">
-					</ul>
-        </div>
+				<div class="menu__inner">
+					<h2 class="menu__title">Our Menus</h2>
+					<div class="menu__foods">
+						<h3 class="food__title">Foods:</h3>
+						<ul class="food__list">
+						</ul>
+					</div>
+					<div class="menu__drinks">
+						<h3 class="drink__title">Drinks:</h3>
+						<ul class="drink__list">
+						</ul>
+					</div>
+				</div>
       </section>
     `;
 	}
