9420412fdfc6c4a610cb358f6389c1333275a3aa
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-10-30 14:23:50 +0700

    feat: add proxy

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,19 @@
-const { merge } = require('webpack-merge');
-const path = require('path');
-const common = require('./webpack.common');
+const { merge } = require('webpack-merge')
+const path = require('path')
+const common = require('./webpack.common')
 
 module.exports = merge(common, {
 	mode: 'development',
 	devServer: {
 		contentBase: path.resolve(__dirname, 'dist'),
 		writeToDisk: true,
+		proxy: {
+			'/api': {
+				target: 'https://restaurant-api.dicoding.dev',
+				changeOrigin: true,
+				secure: true,
+				pathRewrite: { '^/api': '' },
+			},
+		},
 	},
-});
+})
