ce30387ca23540d9ab2ed39a284c8157e5095f78
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-10-30 14:06:53 +0700

    fix: service worker method 405

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,10 +1,21 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
+workbox.core.skipWaiting()
+workbox.core.clientsClaim()
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
-	ignoreURLParametersMatching: [/.*/],
-});
+// Precache
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || [])
 
-workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate());
+// Only cache specific asset types, not APIs
+workbox.routing.registerRoute(
+	({ request }) =>
+		request.destination === 'style' ||
+		request.destination === 'script' ||
+		request.destination === 'image',
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: 'assets-cache',
+	})
+)
+
+// Let all other requests (like API calls) go straight to the network
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly())
